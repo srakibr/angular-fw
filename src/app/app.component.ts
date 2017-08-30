478146d37ea837a36b11921a831f41e123e9a189
srakibr <rakib.rahman@selise.ch>
2017-08-30 15:49:06 +0600

    angular framework initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FrameworkConfigService,FrameworkConfigSettings} from '../fw/services/framework-config.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private frameworkConfigService:FrameworkConfigService){
+    let config:FrameworkConfigSettings = {
+      socialIcons:[
+        {imageFile:'assets/social-fb-bw.png',alt:'Facebook',link:'facebook.com'},
+        {imageFile:'assets/social-google-bw.png',alt:'Google',link:'google.com'},
+        {imageFile:'assets/social-twitter-bw.png',alt:'Twitter',link:'twitter.com'}
+      ],
+      showLanguageSelector:true,
+      showUserControls:false,
+      showStatusBar:true,
+      showStatusBarBreakPoint:800
+    }
+
+    frameworkConfigService.configure(config);
+  }
 }
